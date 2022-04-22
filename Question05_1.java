9c58dca29b635e81023a6a60d6bbcbc26686ba28
doaram123 <104178720+doaram123@users.noreply.github.com>
2022-04-22 11:08:58 +0900

    Update Question05_1.java

--- Question05_1.java
+++ Question05_1.java
@@ -5,7 +5,8 @@ public class Question05_1 {
 	public static void main(String[] args) {
 		int num = 120;
 		if(num>0 && (num%2)==0) {
-			System.out.println("����̸鼭 ¦��");
+			System.out.println("¾ç¼öÀÌ¸é¼­ Â¦¼ö");
+			System.out.pirntln("안녕하세요")
 		}
 
 	}
