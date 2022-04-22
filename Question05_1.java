63d1500c752901ac822ddbf39ce8a342d9acbc3b
doaram123 <doaram123@naver.com>
2022-04-22 11:40:18 +0900

    반갑습니다.

--- Question05_1.java
+++ Question05_1.java
@@ -1,14 +1,1 @@
-package chapter05;
-
-public class Question05_1 {
-
-	public static void main(String[] args) {
-		int num = 120;
-		if(num>0 && (num%2)==0) {
-			System.out.println("¾ç¼öÀÌ¸é¼­ Â¦¼ö");
-			System.out.pirntln("안녕하세요")
-		}
-
-	}
-
-}
+반갑습니다.
