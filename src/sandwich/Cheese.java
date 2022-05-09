84dbfd045199e065c7d457b43bf58a12673c954c
doaram123 <104178720+doaram123@users.noreply.github.com>
2022-05-09 16:05:25 +0900

    Add files via upload

---
+++ src/sandwich/Cheese.java
@@ -0,0 +1,23 @@
+package sandwich;
+
+import java.util.Scanner;
+
+public class Cheese {
+	public static void selectCh(String ch) {
+		System.out.println(ch);
+//		String[] ch = {"1.아메리칸치즈", "2.슈레드치즈", "3.모차렐라치즈", "4.치즈 제외"};
+//		
+//		System.out.println("치즈를 선택하세요.");
+//		
+//		for(int i=0;i<ch.length;i++) {
+//			System.out.println(ch[i]);
+//		}
+//		
+//		System.out.println();
+//		System.out.print("치즈 선택 : ");
+//		Scanner sc=new Scanner(System.in);
+//		int num1=sc.nextInt();
+		
+		
+	}
+}
---
+++ src/sandwich/MenuName.java
@@ -0,0 +1,17 @@
+package sandwich;
+
+public class MenuName {
+	
+	static void sandwichMenu(String na, int pr1, int pr2) {
+		System.out.println(na+" - 15cm: "+pr1+"원 30cm: "+pr2+"원");
+	}
+}
+		
+	
+//	String[] mn=new String[] {"1.베지 아보카도", "2.로스트 치킨 아보카도", "3.터키 베이컨 아보카도", "4.로티세리 바비큐 치킨",
+//		"5.스테이크&치즈", "6.K-바비큐", "7.폴드 포크 바비큐", "8.써브웨이 클럽", "9.쉬림프", "10.스파이시 이탈리안", "11.치킨 데리야끼",
+//		"12.비엘티", "13.이탈리안비엠티", "14.미트볼", "15.터키", "16.참치", "17.에그마요", "18.베지"};
+//	
+//	public String[] nameSand() {
+//		return mn;
+//	}
