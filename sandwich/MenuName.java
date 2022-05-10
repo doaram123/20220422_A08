9dc424a9a5cc9dd92e4e2c94fdb2107632200985
doaram123 <104178720+doaram123@users.noreply.github.com>
2022-05-10 13:59:52 +0900

    Add files via upload

---
+++ sandwich/MenuName.java
@@ -0,0 +1,61 @@
+package sandwich;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MenuName {
+	
+//	public void sandwichMenu() {
+//		System.out.println("<샌드위치 메뉴>");
+//		System.out.println("1.베지 아보카도");
+//		System.out.println("2.로스트 치킨 아보카도");
+//		System.out.println("3.터키 베이컨 아보카도");
+//		System.out.println("4.로티세리 바비큐 치킨");
+//		System.out.println("5.스테이크&치즈");
+//		System.out.println("6.K-바비큐");
+//		System.out.println("7.폴드 포크 바비큐");
+//		System.out.println("8.써브웨이 클럽");
+//		System.out.println("9.쉬림프");
+//		System.out.println("10.스파이스 이탈리안");
+//		System.out.println("11.치킨 데리야끼");
+//		System.out.println("12.비엘티");
+//		System.out.println("13.이탈리안비엠티");
+//		System.out.println("14.미트볼");
+//		System.out.println("15.터키");
+//		System.out.println("16.참치");
+//		System.out.println("17.에그마요");
+//		System.out.println("18.베지");
+//	}
+	
+	static void sandwichMenu(String na, int pr1, int pr2) {
+		System.out.println(na+" - 15cm: "+pr1+"원 30cm: "+pr2+"원");
+	}
+	
+		
+//	public List<String> sandList=new ArrayList<>(Arrays.asList("1.베지 아보카도","2.로스트 치킨 아보카도","3.터키 베이컨 아보카도","4.로티세리 바비큐 치킨",
+//			"5.스테이크&치즈","6.K-바비큐","7.폴드 포크 바비큐","8.써브웨이 클럽","9.쉬림프","10.스파이스 이탈리안","11.치킨 데리야끼","12.비엘티","13.이탈리안비엠티",
+//			"14.미트볼","15.터키","16.참치","17.에그마요","18.베지"));
+//	String result="";
+	
+//	public void sandName() {
+//		sandList.add("1.베지 아보카도");
+//		sandList.add("2.로스트 치킨 아보카도");
+//		sandList.add("3.터키 베이컨 아보카도");
+//		sandList.add("4.로티세리 바비큐 치킨");
+//		sandList.add("5.스테이크&치즈");
+//		sandList.add("6.K-바비큐");
+//		sandList.add("7.폴드 포크 바비큐");
+//		sandList.add("8.써브웨이 클럽");
+//		sandList.add("9.쉬림프");
+//		sandList.add("10.스파이스 이탈리안");
+//		sandList.add("11.치킨 데리야끼");
+//		sandList.add("12.비엘티");
+//		sandList.add("13.이탈리안비엠티");
+//		sandList.add("14.미트볼");
+//		sandList.add("15.터키");
+//		sandList.add("16.참치");
+//		sandList.add("17.에그마요");
+//		sandList.add("18.베지");
+//	}
+}
---
+++ sandwich/OrderCheck.java
@@ -0,0 +1,6 @@
+package sandwich;
+
+
+public class OrderCheck {
+	
+}
