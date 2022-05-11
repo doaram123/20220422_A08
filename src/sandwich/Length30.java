5503c0665a499e2a8426df41967e808b9d024c0d
doaram123 <104178720+doaram123@users.noreply.github.com>
2022-05-11 12:35:46 +0900

    Delete src/sandwich directory

--- src/sandwich/Length30.java
+++
@@ -1,5 +0,0 @@
-package sandwich;
-
-public class Length30 {
-
-}
--- src/sandwich/Vegetable.java
+++
@@ -1,7 +0,0 @@
-package sandwich;
-
-public class Vegetable {
-	public static void vegetable(String veg) {
-		System.out.print(veg);
-	}
-}
